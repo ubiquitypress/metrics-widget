94ee5d589741ef7426aef0a0e5441bfddae334cb
Rees <reesmorriscontact@gmail.com>
2021-05-07 12:24:59 +0100

    Setup Jest with react-testing-library

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  transform: {
+    '\\.(js|jsx)?$': 'babel-jest'
+  },
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  moduleFileExtensions: ['js', 'jsx'],
+  testPathIgnorePatterns: ['/node_modules/', '/public/'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname'
+  ]
+};
