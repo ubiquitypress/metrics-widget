89a72815723a1f1b6877c7ad19a147d710802958
Rees Morris <git@rees.email>
2025-12-13 00:25:04 +0000

    Wrap React widget in configurable container

--- src/react.tsx
+++ src/react.tsx
@@ -1,3 +1,4 @@
+import type React from 'react';
 import { Main } from './widget';
 import type { EventsMap } from './widget/events';
 import type {
@@ -12,13 +13,24 @@ import type {
 export interface MetricsWidgetProps {
   config: UserConfig;
   events?: Partial<EventsMap>;
+  id?: string;
+  className?: string;
+  containerRef?: React.Ref<HTMLDivElement>;
 }
 
 /**
  * React entrypoint for the metrics widget. Pass a UserConfig and handle the rest as a normal React component.
  */
-export const MetricsWidget = ({ config, events }: MetricsWidgetProps) => (
-  <Main config={config} events={events} />
+export const MetricsWidget = ({
+  config,
+  events,
+  id = 'metrics-widget',
+  className,
+  containerRef
+}: MetricsWidgetProps) => (
+  <div id={id} className={className} ref={containerRef}>
+    <Main config={config} events={events} />
+  </div>
 );
 
 export type { UserConfig, Config, Graph, Graphs, Scope, Tab };
